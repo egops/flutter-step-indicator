8d8f4db471318acbcabf5acb16667c175e7598cb
ydgao <ydgao@live.cn>
2022-10-26 10:25:14 +0800

    区分激活与完成的状态: complete/running fulllinearstepindicator:   删除起始和结尾进度横线   用IntListener替代pageController, 简化代码并获得回退功能   节点内的数字样式可以指定   完成后使用指定图标替换节点内的数字   每个节点激活且未完成时显示progressIndicator   优化每个节点label的显示位置及显示效果

--- lib/src/full_linear_step_indicator.dart
+++ lib/src/full_linear_step_indicator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:garagelock/view/components/adjustImg.dart';
+import 'package:garagelock/view/components/autoRotate.dart';
 import 'package:linear_step_indicator/src/constants.dart';
 import 'extensions.dart';
 import 'node.dart';
@@ -8,7 +10,7 @@ class FullLinearStepIndicator extends StatefulWidget {
   ///
   ///Typically, controller should animate or jump to next page
   ///when a step is completed
-  final PageController controller;
+  final IntListener controller;
 
   ///Number of nodes to paint on screen
   final int steps;
@@ -31,17 +33,35 @@ class FullLinearStepIndicator extends StatefulWidget {
   ///Color of each uncompleted node border
   final Color inActiveBorderColor;
 
+  ///Color of each uncompleted node border
+  final Color completeBorderColor;
+
+  ///Color of each completed node border
+  final Color activeFlagColor;
+
+  ///Color of each uncompleted node border
+  final Color inActiveFlagColor;
+
+  ///Color of each uncompleted node border
+  final Color completeFlagColor;
+
   ///Color of each separating line after a completed node
   final Color activeLineColor;
 
   ///Color of each separating line after an uncompleted node
   final Color inActiveLineColor;
 
+  ///Color of each uncompleted node border
+  final Color completeLineColor;
+
   ///Background color of a completed node
   final Color activeNodeColor;
 
   ///Background color of an uncompleted node
   final Color inActiveNodeColor;
+
+  ///Color of each uncompleted node border
+  final Color completeNodeColor;
 
   ///Thickness of node's borders
   final double nodeThickness;
@@ -76,10 +96,16 @@ class FullLinearStepIndicator extends StatefulWidget {
     required this.controller,
     this.activeBorderColor = kActiveColor,
     this.inActiveBorderColor = kInActiveColor,
+    this.completeBorderColor = kCompleteColor,
     this.activeLineColor = kActiveLineColor,
     this.inActiveLineColor = kInActiveLineColor,
+    this.completeLineColor = kCompleteColor,
     this.activeNodeColor = kActiveColor,
     this.inActiveNodeColor = kInActiveNodeColor,
+    this.completeNodeColor = kCompleteColor,
+    this.activeFlagColor = kActiveColor,
+    this.inActiveFlagColor = kInActiveColor,
+    this.completeFlagColor = kCompleteColor,
     this.iconSize = kIconSize,
     this.completedIcon = kCompletedIcon,
     this.nodeThickness = kDefaultThickness,
@@ -94,13 +120,42 @@ class FullLinearStepIndicator extends StatefulWidget {
     this.activeLabelStyle,
     this.inActiveLabelStyle,
   })  : assert(steps > 0, "steps value must be a non-zero positive integer"),
-        assert(labels.length == steps || labels.length == 0,
-            "Provide exactly $steps strings for labels"),
+        assert(labels.length == steps || labels.length == 0, "Provide exactly $steps strings for labels"),
         super(key: key);
 
   @override
-  _FullLinearStepIndicatorState createState() =>
-      _FullLinearStepIndicatorState();
+  _FullLinearStepIndicatorState createState() => _FullLinearStepIndicatorState();
+}
+
+class IntListener {
+  VoidCallback? _listener;
+
+  var _page = 0;
+
+  get page {
+    return _page;
+  }
+
+  set page(value) {
+    _page = value;
+    if (_listener != null) _listener!();
+  }
+
+  void addListener(VoidCallback listener) {
+    _listener = listener;
+  }
+
+  void nextPage() {
+    this.page = _page + 1;
+  }
+
+  void prevPage() {
+    this.page = _page - 1;
+  }
+
+  IntListener({initialValue = 0}) {
+    this._page = initialValue;
+  }
 }
 
 class _FullLinearStepIndicatorState extends State<FullLinearStepIndicator> {
@@ -112,29 +167,23 @@ class _FullLinearStepIndicatorState extends State<FullLinearStepIndicator> {
     super.initState();
     nodes = List<Node>.generate(widget.steps, (index) => Node(step: index));
     lastStep = 0;
+    nodes[lastStep].isRunning = true;
 
     //listen to page changes to track when each step is ideally completed
 
     widget.controller.addListener(
       () async {
-        if (widget.controller.page! > lastStep) {
-          setState(
-            () {
-              nodes[lastStep].completed = true;
-              lastStep = widget.controller.page!.ceil();
-            },
-          );
-        }
+        setStep(widget.controller.page!);
+      },
+    );
+  }
 
-        //checks if the controller has hit the max step
-        //and checks [complete] to complete last node (or not)
-
-        if (widget.controller.page! == widget.steps - 1 &&
-            widget.complete != null) {
-          if (await widget.complete!()) {
-            nodes[widget.steps - 1].completed = true;
-            setState(() {});
-          }
+  void setStep(int step) {
+    setState(
+      () {
+        for (int i = 0; i < widget.steps; i++) {
+          nodes[i].completed = (i < step);
+          nodes[i].isRunning = (i == step);
         }
       },
     );
@@ -154,63 +203,83 @@ class _FullLinearStepIndicatorState extends State<FullLinearStepIndicator> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 for (var node in nodes) ...[
+                  // 第一条横线
                   if (nodes.indexOf(node) == 0) ...{
                     Container(
-                      color: node.completed
-                          ? widget.activeLineColor
-                          : widget.inActiveLineColor,
-                      height: widget.lineHeight,
+                      // color: node.completed
+                      //     ? widget.activeLineColor
+                      //     : widget.inActiveLineColor,
+                      // height: widget.lineHeight,
                       width: context.screenWidth(1 / widget.steps) * .25,
                     ),
                   },
-                  Container(
-                    alignment: Alignment.center,
-                    height: 38,
-                    width: 38,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: node.completed
-                          ? widget.activeNodeColor
-                          : widget.inActiveNodeColor,
-                      border: Border(
-                        top: borderSide,
-                        bottom: borderSide,
-                        left: borderSide,
-                        right: borderSide,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          spreadRadius: .5,
-                          blurRadius: .5,
-                          color: Theme.of(context)
-                              .primaryColorDark
-                              .withOpacity(.4),
+                  // 节点
+                  Stack(
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 38,
+                        width: 38,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: node.completed
+                              ? widget.completeNodeColor
+                              : (node.isRunning ? widget.activeNodeColor : widget.inActiveNodeColor),
+                          // 白色圈圈
+                          border: Border(
+                            top: borderSide,
+                            bottom: borderSide,
+                            left: borderSide,
+                            right: borderSide,
+                          ),
+                          // 最外层阴影
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: .5,
+                              blurRadius: .5,
+                              color: Theme.of(context).primaryColorDark.withOpacity(.4),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: Text(
-                      "${nodes.indexOf(node) + 1}",
-                      style: TextStyle(
-                        color: Colors.white,
+                        // 圈内部文字
+                        child: node.completed
+                            ? Icon(
+                                widget.completedIcon,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "${nodes.indexOf(node) + 1}",
+                                style: TextStyle(
+                                  color: node.completed
+                                      ? widget.completeFlagColor
+                                      : (node.isRunning ? widget.activeFlagColor : widget.inActiveFlagColor),
+                                ),
+                              ),
                       ),
-                    ),
+                      // 转圈圈
+                      if (node.isRunning) ...{
+                        SizedBox(
+                          height: 38,
+                          width: 38,
+                          child: AutoRotate(child: AdjustImg("assets/images/load.png")),
+                        ),
+                      },
+                    ],
                   ),
+                  // 两个节点中间的横线
                   if (node.step != widget.steps - 1)
                     Container(
-                      color: node.completed
-                          ? widget.activeLineColor
-                          : widget.inActiveLineColor,
+                      color: node.completed ? widget.activeLineColor : widget.inActiveLineColor,
                       height: widget.lineHeight,
-                      width: widget.steps > 3
-                          ? context.screenWidth(1 / widget.steps) - 40
-                          : context.screenWidth(1 / widget.steps) - 28,
+                      width: widget.steps > 3 ? context.screenWidth(1 / widget.steps) - 40 : context.screenWidth(1 / widget.steps) - 28,
                     ),
+                  // 最后一条横线
                   if (nodes.indexOf(node) == widget.steps - 1) ...{
                     Container(
-                      color: node.completed
-                          ? widget.activeLineColor
-                          : widget.inActiveLineColor,
-                      height: widget.lineHeight,
+                      // color: node.completed
+                      //     ? widget.activeLineColor
+                      //     : widget.inActiveLineColor,
+                      // height: widget.lineHeight,
                       width: context.screenWidth(1 / widget.steps) * .25,
                     ),
                   },
@@ -219,26 +288,32 @@ class _FullLinearStepIndicatorState extends State<FullLinearStepIndicator> {
             ),
             SizedBox(height: 6),
             if (widget.labels.length > 0) ...[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  for (int i = 0; i < widget.labels.length; i++) ...[
-                    Text(
-                      widget.labels[i],
-                      style: nodes[i].completed
-                          ? widget.activeLabelStyle
-                          : widget.inActiveLabelStyle,
-                    ),
-                    if (widget.labels[i] !=
-                        widget.labels[widget.steps - 1]) ...[
+              Container(
+                margin: EdgeInsets.only(left: context.screenWidth(1 / widget.steps) * .25 - 12, right: context.screenWidth(1 / widget.steps) * .25 - 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (int i = 0; i < widget.labels.length; i++) ...[
                       SizedBox(
-                        width: widget.steps > 3
-                            ? context.screenWidth(1 / widget.steps) - 45
-                            : context.screenWidth(1 / widget.steps) - 35,
+                        width: 70,
+                        child: Text(
+                          widget.labels[i],
+                          textAlign: TextAlign.center,
+                          style: (nodes[i].completed || nodes[i].isRunning) ? widget.activeLabelStyle : widget.inActiveLabelStyle,
+                        ),
                       ),
+                      // if (widget.labels[i] !=
+                      //     widget.labels[widget.steps - 1]) ...[
+                      //   SizedBox(
+                      //     width: widget.steps > 3
+                      //         ? context.screenWidth(1 / widget.steps) - 45
+                      //         : context.screenWidth(1 / widget.steps) - 35,
+                      //   ),
+                      // ],
                     ],
                   ],
-                ],
+                ),
               ),
             ],
           ],
--- lib/src/node.dart
+++ lib/src/node.dart
@@ -1,9 +1,11 @@
 class Node {
   bool completed;
+  bool isRunning;
   int step;
 
   Node({
     this.completed = false,
+    this.isRunning = false,
     this.step = 0,
   });
 }
